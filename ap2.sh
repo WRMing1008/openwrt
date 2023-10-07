2edb6b5a11d16160d511acab0790a4d62da2d39f
AE86JAY <AE86JAY@users.noreply.github.com>
2023-10-07 09:27:10 +0800

    Update ap2.sh

--- ap2.sh
+++ ap2.sh
@@ -13,6 +13,5 @@
 sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
 sed -i "s/set system.ntp.enable_server='1'/set system.ntp.enable_server='0'/g"
 sed -i "s/hostname='immortalwrt'/hostname='LEDE'/g" ./package/base-files/files/bin/config_generate
-rm -rf /feeds/luci/themes/luci-theme-argon
-sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' ./package/luci/collections/luci-light/Makefile
 sed -i 's|<a href="https://github.com/openwrt/luci">Powered by <%|<a href="https://www.right.com.cn/forum/space-uid-671291.html">恩山无线论坛<突破重韦> <%|g' ./feeds/luci/themes/luci-theme-argon/luasrc/view/themes/argon/footer.htm
